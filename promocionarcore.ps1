8448343eab97344730b5077e7081fa3426d7d2b6
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-03-29 16:33:54 +0200

    Update promocionarcore.ps1

--- promocionarcore.ps1
+++ promocionarcore.ps1
@@ -1,2 +1,7 @@
 #Lo primero es importar el modulo con el que voy a administrar el servidor
 Import-Module ServerManager
+
+#Ahora tengo que añadir este comando que instala servicios de directorios
+#que proporciona los métodos para almacenar datos de directorio 
+#y hace que estos datos estén disponibles para los usuarios y administradores de la red
+Add-WindowsFeature AD-Domain-Services
