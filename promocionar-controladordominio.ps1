07c819bf6a25d72dce737234dc734e225479d8a0
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-03-30 00:28:47 +0200

    Update promocionar-controladordominio.ps1

--- promocionar-controladordominio.ps1
+++ promocionar-controladordominio.ps1
@@ -16,3 +16,12 @@ Install-ADDSForest `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
+
+#Esto es lo que quiere decir
+#Domain Mode y -ForestMode
+#Windows Server 2003: 2 or Win2003
+#Windows Server 2008: 3 or Win2008
+#Windows Server 2008 R2: 4 or Win2008R2
+#Windows Server 2012: 5 or Win2012
+#Windows Server 2012 R2: 6 or Win2012R2
+#Windows Server 2016: 7 or WinThreshold
