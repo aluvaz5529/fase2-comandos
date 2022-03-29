f09a7223b39c796917a5d0ed29ad66a413f07390
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-03-29 22:40:58 +0200

    Create promocionar-controladordominio.ps1

---
+++ promocionar-controladordominio.ps1
@@ -0,0 +1,18 @@
+#script para crear un nuevo busque junto a el dominio raiz
+#
+# Script de Windows PowerShell para implementación de AD DS
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "san-gva.es" `
+-DomainNetbiosName "SAN-GVA" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
