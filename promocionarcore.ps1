d1b422f92b8b59b270bdc3838aa53e4015c3c44b
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-03-29 16:38:56 +0200

    Update promocionarcore.ps1

--- promocionarcore.ps1
+++ promocionarcore.ps1
@@ -5,3 +5,11 @@ Import-Module ServerManager
 #que proporciona los métodos para almacenar datos de directorio 
 #y hace que estos datos estén disponibles para los usuarios y administradores de la red
 Add-WindowsFeature AD-Domain-Services
+#Ahora compruebo que el modulo ADDSDeployment está, y si no lo añado
+if (!(Get-Module -Name ADDSDeployment))
+#con este comando lo añado
+{
+  Import-Module ADDSDeployment #Se carga el módulo
+}
+#Este modulo añade estos comandos https://docs.microsoft.com/es-es/powershell/module/addsdeployment/?view=windowsserver2022-ps&viewFallbackFrom=win10-ps
+#Una vez añadido todos los modulos y características ya podemos pasar a promocionar como adicional al controlador replica
