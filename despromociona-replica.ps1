c606c8dea4d2609849d8a20f681b764ca097bc1d
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-05 16:39:22 +0200

    Update despromociona-replica.ps1

--- despromociona-replica.ps1
+++ despromociona-replica.ps1
@@ -1,7 +1,14 @@
-#antes de eliminarlo hay que comprobar
+#Antes de eliminarlo hay que comprobar
 #Para comprobar Estado del salud Controlador de Dominio (replica)
 dcdiag /c /e /v
-#parametros
+#Parametros
 #/c hace todas las pruebas
 #/e prueba todos los servidores de la empresa
 #/v te hace un informe detallado
+
+#Desintalar el ADDSDomaincontroller AD-Domain-Services y el DNS
+Uninstall-ADDSDomainController `
+-DemoteOperationMasterRole:$true `
+-ForceRemoval:$true `
+-Force:$true
+Uninstall-WindowsFeature -Name AD-Domain-Services, DNS -Confirm:$false
