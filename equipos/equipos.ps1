225f0a573bfb126abfa16d44c9a54af42d28d138
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-13 00:51:15 +0200

    Update equipos.ps1

--- equipos/equipos.ps1
+++ equipos/equipos.ps1
@@ -1,15 +1,8 @@
-$dominio=san-gva.es
 $dc="dc=hgeneral,dc=san-gva,dc=es"
 $equiposCsv=Read-Host "Introduce el fichero csv de Equipos"
 $fichero= import-csv -Path $equiposCsv -delimiter ":"
 
 foreach($linea in $fichero)
 {
-	$pathObject=$linea.Path+","+$dc	
-	#Comprobamos que no exista el equipo en el sistema
-	if ( !(Get-ADComputer -Filter { name -eq $linea.Computer }) )
-	{
-		New-ADComputer -Enabled:$true -Name:$linea.Computer -Path:$pathObject -SamAccountName:$linea.Computer
-	}
-	else { Write-Host "El equipo $linea.Computer ya existe en el sistema"}
+New-ADComputer -Enabled:$true -Name:$linea.Computer -Path:$linea.Path -SamAccountName:$linea.Computer
 }
