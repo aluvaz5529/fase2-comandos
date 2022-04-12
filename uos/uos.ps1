805ad6ae8a1997c2abdb6a3210a9317eb615c740
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-12 17:26:24 +0200

    Update uos.ps1

--- uos/uos.ps1
+++ uos/uos.ps1
@@ -7,3 +7,5 @@ foreach($line in $fichero)
 {
    New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name -Path:$domainComponent -ProtectedFromAccidentalDeletion:$true
 }
+#El parametro del final quiere decir que si quieres que se proteja para que no lo borres sin querer ,pero si quieres hacer pruebas mejor false porque tendrás
+#que borrarlo bastantes veces
