54c32dd3ba6ea4dd232eff897dd7e722043c2481
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-12 23:23:26 +0200

    Update uos.ps1

--- uos/uos.ps1
+++ uos/uos.ps1
@@ -1,11 +1,12 @@
-param($dominio,$suf)
-$domainComponent="dc="+$dominio+",dc="+$suf
-Write-Host $domainComponent
+$dominio=san-gva.es
+$dc="dc=hgeneral,dc=san-gva,dc=es"
+Write-Host $dc
+$Path=$linea.Path+","+$dc	
 $ficheroCsvUO=Read-Host "Introduce el fichero csv de UO's:"
 $fichero = import-csv -Path $ficheroCsvUO -delimiter :
-foreach($line in $fichero)
+foreach($linea in $fichero)
 {
-   New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name -Path:$domainComponent -ProtectedFromAccidentalDeletion:$true
+   New-ADOrganizationalUnit -Description:$linea.Description -Name:$linea.Name -Path:$Path -ProtectedFromAccidentalDeletion:$true
 }
 #El parametro del final quiere decir que si quieres que se proteja para que no lo borres sin querer ,pero si quieres hacer pruebas mejor false porque tendrás
 #que borrarlo bastantes veces
