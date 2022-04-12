79b877af977c14ef59004762e03f55a56dc0aef9
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-12 17:13:53 +0200

    Update crear-usuario.ps1

--- usuarios/crear-usuario.ps1
+++ usuarios/crear-usuario.ps1
@@ -1,5 +1,5 @@
 $dominio=hgeneral.san-gva.es
-#En la variable dc componemos el nombre dominio y sufijo. Ejemplo: dc=san-gva,dc=es.
+#Creas una variable con el nombre del dominio Ejemplo: dc=san-gva,dc=es.
 $dc="dc="+$dominio+",dc="+$sufijo
 if (!(Get-Module -Name ActiveDirectory)) #accederá al then si no tiene ActiveDirectory sería como decirle al ordenador si no lo tienes carga el modulo,si lo tienes no hagas nada
 {
